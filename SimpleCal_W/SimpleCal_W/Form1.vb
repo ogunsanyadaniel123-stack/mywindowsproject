b846df62b46a412253d9ab71af4f25cfc66c0379
daniel <ogunsanyadaniel123@gmail.com>
2025-12-16 12:58:16 +0100

    simpleCal_W was updated on 16/12/2025 @ 12:58 pm

---
+++ SimpleCal_W/SimpleCal_W/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+ 
+    Private Sub sumCalculatebtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sumCalculatebtn.Click
+        Dim firstNumber, secondNumber, result As Double
+        firstNumber = firstNumber_txt.Text
+        secondNumber = secondNumber_txt.Text
+
+        result = firstNumber + secondNumber
+        result_txt.Text = result
+
+    End Sub
+End Class
